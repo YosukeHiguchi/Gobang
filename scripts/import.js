e174e3bd51aa729befebd4226b57c58a4086e49a
YosukeHiguchi <wasapon.dev@gmail.com>
2016-10-09 17:09:22 +0900

    Delete import.js

--- scripts/import.js
+++
@@ -1,5 +0,0 @@
-document.write('<script type="text/javascript" src="scripts/canvas.js"></script>');
-document.write('<script type="text/javascript" src="scripts/grid.js"></script>');
-document.write('<script type="text/javascript" src="scripts/main.js"></script>');
-/*------------AI-----------*/
-document.write('<script type="text/javascript" src="lib/zetaGo.js"></script>');
