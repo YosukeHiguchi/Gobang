3a7dc99ddcb90d8c6e66e42d2e1d6e44ba4195df
Yosuke Higuchi <yosukehiguchi@Yosukes-MacBook-Air.local>
2016-10-09 18:34:38 +0900

    some fixes

--- scripts/main.js
+++ scripts/main.js
@@ -27,6 +27,12 @@ document.onclick = function (e) {
     (id == 1)? id = 2: id = 1;
   }
 
+  //for human vs human
+  // if (x_grid >= 0 && y_grid >= 0 && x_grid <= 18 && y_grid <= 18 && grid[y_grid][x_grid] == 0) {
+  //   placeStone(x_grid, y_grid);
+  //   (id == 1)? id = 2: id = 1;
+  // }
+
   var winner = getWinner();
   if (winner == 1) {
     alert("black won");
