e1afdd4b6a57dde1e907eb62448beb5a9bca89e4
Yosuke Higuchi <yosukehiguchi@Yosukes-MacBook-Air.local>
2016-10-10 15:52:44 +0900

    fix game system

--- scripts/main.js
+++ scripts/main.js
@@ -18,27 +18,31 @@ document.onclick = function (e) {
   /*AI vs human (human always first for now)*/
   if (x_grid >= 0 && y_grid >= 0 && x_grid <= 18 && y_grid <= 18 && grid[y_grid][x_grid] == 0) {
     placeStone(x_grid, y_grid);
+    isGameOver();
     (id == 1)? id = 2: id = 1;
+
 
     /*---AI----*/
     zetaGo();
     /*---------*/
 
+
+    isGameOver();
     (id == 1)? id = 2: id = 1;
   }
 
   /*human vs human*/
   // if (x_grid >= 0 && y_grid >= 0 && x_grid <= 18 && y_grid <= 18 && grid[y_grid][x_grid] == 0) {
   //   placeStone(x_grid, y_grid);
+  //   isGameOver();
   //   (id == 1)? id = 2: id = 1;
   // }
 
   /*AI vs AI 調整中*/
   // betaGo();
+  // isGameOver();
+  // (id == 1)? id = 2: id = 1;
   // zetaGo();
-
-  var winner = getWinner();
-  if (winner != 0) {
-    (winner == 1)? alert("black won"): alert("white won");
-  }
+  // isGameOver();
+  // (id == 1)? id = 2: id = 1;
 }
--- scripts/system.js
+++ scripts/system.js
@@ -1,15 +1,16 @@
-function getWinner() {
+function isGameOver() {
   for (var i = 0; i < 19; i++) {
     for (var j = 0; j < 19; j++) {
       if ((isEq(i, j, i-1, j) && isEq(i, j, i-2, j) && isEq(i, j, i+1, j) && isEq(i, j, i+2, j)) ||
           (isEq(i, j, i, j-1) && isEq(i, j, i, j-2) && isEq(i, j, i, j+1) && isEq(i, j, i, j+2)) ||
           (isEq(i, j, i-1, j-1) && isEq(i, j, i-2, j-2) && isEq(i, j, i+1, j+1) && isEq(i, j, i+2, j+2)) ||
           (isEq(i, j, i-1, j+1) && isEq(i, j, i-2, j+2) && isEq(i, j, i+1, j-1) && isEq(i, j, i+2, j-2))) {
-            return grid[i][j];
+            (grid[i][j] == 1)? alert("black won"): alert("white won");
+            return true;
       }
     }
   }
-  return 0;
+  return false;
 
   function isEq(i, j, a, b) {
     if (a < 0 || b < 0 || a > 18 || b > 18) return false;
